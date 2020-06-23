11371395c00f23ff5766ce9c700c3da9e7d55017
Adrian1608 <eladrian1608@gmail.com>
2020-06-23 10:45:17 -0500

    Actualizacion

--- view/busqueda.php
+++ view/busqueda.php
@@ -4,11 +4,12 @@
 <?php
     if(isset($_POST["busqueda"])){
         $busqueda = ': ' . $_POST["busqueda"];
-    }else{
+    }else{ ## Esta parte es para que el titulo diga por ejemplo "Buscando en TriviZuz: Zapatillas"
         $busqueda = "";
     }
 ?>
     <title>Buscando en Trivizuz<?php echo $busqueda;?> </title>
+    <link rel="icon" href="../img/3vzuz icono.ico">
 </head>
 
 <form method="post" action="busqueda.php">
@@ -16,8 +17,7 @@
     <button type="submit" value="Buscar">Buscar</button>
 </form>
 
-<br>
-<p>Que conste porfavor que esto aun es lo super basico:B</p>
+
 
 <?php
     ##Esta será la página de búsqueda
