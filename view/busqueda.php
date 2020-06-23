91b7176601b551180de86cd5ff64af9c75961144
Adrian1608 <eladrian1608@gmail.com>
2020-06-22 20:43:31 -0500

    Pagina de busqueda

--- view/busqueda.php
+++ view/busqueda.php
@@ -1,7 +1,27 @@
+<html>
+<meta charset="UTF-8">
+<head>
+<?php
+    if(isset($_POST["busqueda"])){
+        $busqueda = ': ' . $_POST["busqueda"];
+    }else{
+        $busqueda = "";
+    }
+?>
+    <title>Buscando en Trivizuz<?php echo $busqueda;?> </title>
+</head>
+
+<form method="post" action="busqueda.php">
+    <input type="text" placeholder="Buscar producto..." name="busqueda">
+    <button type="submit" value="Buscar">Buscar</button>
+</form>
+
+<br>
+<p>Que conste porfavor que esto aun es lo super basico:B</p>
+
 <?php
     ##Esta será la página de búsqueda
-
-
-
-
 ?>
+
+
+</html>
